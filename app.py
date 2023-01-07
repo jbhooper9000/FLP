ca7bff5731836382806f3cf87f1ecb381dd2d707
jbhooper9000 <79251916+jbhooper9000@users.noreply.github.com>
2023-01-07 16:53:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -164,8 +164,7 @@ p_all = pd.concat([p1,p2,p3,p4,p5], axis=1)
 fig_ts = go.Figure()
 mask = []
 for i in range(int(len(p_all.columns)/len(p1.columns))):
-  try:
-    for j in range(i*len(p1.columns),len(p1.columns)+i*len(p1.columns)):
+  for j in range(i*len(p1.columns),len(p1.columns)+i*len(p1.columns)):
       fig_ts.add_trace(go.Scatter(
                       x = p_all.index,
                       y = p_all.iloc[:,j],
@@ -173,8 +172,6 @@ for i in range(int(len(p_all.columns)/len(p1.columns))):
                       )
         )
       mask.append(i)
-  except ZeroDivisionError:
-    pass
 fig_ts.update_layout(
     updatemenus=[go.layout.Updatemenu(
         active=0,
