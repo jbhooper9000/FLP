acee29c8743075e4293c93d6f9def224be80510a
jbhooper9000 <79251916+jbhooper9000@users.noreply.github.com>
2023-01-28 14:56:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ assets = asset_container.slider('Asset Value',
                                 assetlist.min(), 
                                 assetlist.max(), 
                                 (float((assetlist.quantile(0.25),assetlist.quantile(0.25))))                                                                                        )
-                                 )
+                                )
 
 case_type = sidebarfilter(df_solicitor, 'case_type', '**Case Type**')
 dr_used = sidebarfilter(df_solicitor, 'dr_used', '**DR Used**')
